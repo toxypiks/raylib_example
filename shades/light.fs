0dd116714bbbab43b3f82390cf69420e8f8fb31a
toxypiks <HannaHinkelbein89@gmx.de>
2024-10-15 10:19:26 +0200

    Use gl_FragCoord to implement light shader

--- shades/light.fs
+++ shades/light.fs
@@ -1,8 +1,8 @@
 #version 330
 
+in vec3 vertexPos;
 in vec2 fragTexCoord;
 in vec4 fragColor;
-in vec4 fragCoord;
 
 out vec4 finalColor;
 
@@ -10,5 +10,5 @@ uniform vec3 cameraPos;
 
 void main() {
     //let color change when camera position does
-    finalColor = vec4(normalize(cameraPos), 1.0);
+    finalColor = gl_FragCoord;
 }
